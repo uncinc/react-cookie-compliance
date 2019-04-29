0890a782282414e5a55aacb616c447a375b24aee
Kim <kim@uncinc.nl>
2019-04-29 10:06:18 +0200

    fix: Set expire-date to ensure Chrome & Safari will not destroy our cookie

--- src/actions/cookieCompliance.js
+++ src/actions/cookieCompliance.js
@@ -2,7 +2,9 @@ import Cookies from 'js-cookie';
 
 export const COOKIE_COMPLIANCE_CONSENT = 'COOKIE_COMPLIANCE_CONSENT';
 export function cookieComplianceConsent(didConsent) {
-  Cookies.set('cookie-compliance-consent', didConsent);
+  Cookies.set('cookie-compliance-consent', didConsent, {
+    expires: 365,
+  });
   return {
     type: COOKIE_COMPLIANCE_CONSENT,
     didConsent,
